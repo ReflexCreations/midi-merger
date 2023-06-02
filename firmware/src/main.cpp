8199f1c5119ef3cd6537294513bbb6eb29844ae3
Michael Duerinckx <mich@michd.me>
2023-06-02 18:19:44 +0100

    Firmware: map inputs to pins in hardware design

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -6,9 +6,15 @@
 #include <stdint.h>
 #include <Arduino.h>
 
-#define PIN_MIDI_SOURCE_A 27
-#define PIN_MIDI_SOURCE_B 26
-#define PIN_MIDI_SOURCE_C 22
+// Mapping as per rev0 of the PCB
+#define PIN_MIDI_SOURCE_1 16
+#define PIN_MIDI_SOURCE_2 17
+#define PIN_MIDI_SOURCE_3 21
+#define PIN_MIDI_SOURCE_4 28
+#define PIN_MIDI_SOURCE_5 5
+#define PIN_MIDI_SOURCE_6 10
+#define PIN_MIDI_SOURCE_7 14
+#define PIN_MIDI_SOURCE_8 15
 
 #define PIN_MIDI_SINK_TX 8
 #define PIN_MIDI_SINK_RX 9
@@ -17,9 +23,14 @@ MidiMerger* merger;
 
 void setup() {
   merger = new MidiMerger(new MidiSink(uart1, PIN_MIDI_SINK_TX, PIN_MIDI_SINK_RX));
-  merger->addSource(new MidiSource(PIN_MIDI_SOURCE_A));
-  merger->addSource(new MidiSource(PIN_MIDI_SOURCE_B));
-  merger->addSource(new MidiSource(PIN_MIDI_SOURCE_C));
+  merger->addSource(new MidiSource(PIN_MIDI_SOURCE_1));
+  merger->addSource(new MidiSource(PIN_MIDI_SOURCE_2));
+  merger->addSource(new MidiSource(PIN_MIDI_SOURCE_3));
+  merger->addSource(new MidiSource(PIN_MIDI_SOURCE_4));
+  merger->addSource(new MidiSource(PIN_MIDI_SOURCE_5));
+  merger->addSource(new MidiSource(PIN_MIDI_SOURCE_6));
+  merger->addSource(new MidiSource(PIN_MIDI_SOURCE_7));
+  merger->addSource(new MidiSource(PIN_MIDI_SOURCE_8));
 }
 
 void loop() {
